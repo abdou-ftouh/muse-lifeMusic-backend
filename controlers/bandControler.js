f62996686fab9292c30bdf8b1ca3f60b285f22b9
abdou-ftouh <elftouh13@gmail.com>
2022-05-26 16:50:31 -0400

    Login and Sign Up and Models CRUD functionalty

--- controlers/bandControler.js
+++ controlers/bandControler.js
@@ -4,14 +4,45 @@ const router = express.Router();
 const Band = require('../models/bandModel')
 
 
-router.get("/band", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-      // get all people
+      
         res.json(await Band.find({}));
     } catch (error) {
       //send error
         res.status(400).json(error);
+        console.log(error)
     }
+});
+router.post("/", async (req, res) => {
+  try {
+   
+    res.json(await Band.create(req.body));
+  } catch (error) {
+    //send error
+      res.status(400).json(error);
+      console.log(error)
+
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    res.json(await Band.findOneAndRemove({ _id: req.params.id }));
+  } catch (error) {
+    res.status(400).json(error);
+    console.log(error)
+  }
+
+})
+router.patch("/:id", async(req, res) => {
+ 
+     try {
+       res.json(await  Band.findOneAndUpdate({ _id: req.params.id }, req.body))
+     } catch (error) {
+      res.status(400).json(error);
+      console.log(error)
+     }
 });
 
 
